0d0699c42f962ab87f12e53f2960a346dc2f3d5b
David Betancur <dbetancur@psc.edu>
2024-04-30 13:46:15 -0400

    Updating scripts

--- src/ingest-pipeline/misc/tools/airflow_environments/env_dev.sh
+++ src/ingest-pipeline/misc/tools/airflow_environments/env_dev.sh
@@ -1,3 +1,3 @@
 #set airflow environment
-AF_CONFIG=/hive/users/niddk/dev/ingest-pipeline/src/ingest-pipeline/airflow/airflow.cfg
-AF_HOME=/hive/users/niddk/dev/ingest-pipeline/src/ingest-pipeline/airflow
+AF_CONFIG=/hive/users/niddk/repositories/dev/ingest-pipeline/src/ingest-pipeline/airflow/airflow.cfg
+AF_HOME=/hive/users/niddk/repositories/dev/ingest-pipeline/src/ingest-pipeline/airflow
