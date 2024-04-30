24b2a374404bc9b355a18904a7417f3893d36d4b
David Betancur <dbetancur@psc.edu>
2024-04-30 14:30:15 -0400

    Updating scripts

--- src/ingest-pipeline/misc/tools/regenerate_conda_env.sh
+++ src/ingest-pipeline/misc/tools/regenerate_conda_env.sh
@@ -3,16 +3,16 @@
 #set -x  # for logging and debugging
 
 # This specifies the instance of the environment to be created
-if [ -z "$HUBMAP_INSTANCE" ]; then
-    exit "The environment variable HUBMAP_INSTANCE is not set."
+if [ -z "NIDDK_INSTANCE" ]; then
+    exit "The environment variable NIDDK_INSTANCE is not set."
 fi
-instance="$HUBMAP_INSTANCE"
+instance="$NIDDK_INSTANCE"
 
 # What python version should be used?
-if [ -z "$HUBMAP_PYTHON_VERSION" ]; then
-    exit "The environment variable HUBMAP_PYTHON_VERSION is not set."
+if [ -z "$NIDDK_PYTHON_VERSION" ]; then
+    exit "The environment variable NIDDK_PYTHON_VERSION is not set."
 fi
-python_version="$HUBMAP_PYTHON_VERSION"
+python_version="$NIDDK_PYTHON_VERSION"
 
 # Root directory for newly created conda environments
 conda_env_root="/opt/environments"
@@ -35,31 +35,31 @@ cd $DIR
 
 source source_platform_file.sh
 
-echo $HM_AF_METHOD $HM_AF_ENV_NAME
-if [ "${HM_AF_METHOD}" == 'conda' ] ; then
-    which conda || export PATH=/hive/users/hive/anaconda3/bin:$PATH
+echo $AF_METHOD $AF_ENV_NAME
+if [ "${AF_METHOD}" == 'conda' ] ; then
+    which conda || export PATH=/opt/anaconda3/bin:$PATH
     eval "$(conda shell.bash hook)"
-elif [ "${HM_AF_METHOD}" == 'module_conda' ] ; then
+elif [ "${AF_METHOD}" == 'module_conda' ] ; then
     source /etc/profile.d/modules.sh
     module use /hive/modulefiles
     module load anaconda
     eval "$(conda shell.bash hook)"
 else
-    echo "The config for this platform specifies a HM_AF_METHOD which is not one of 'conda' or 'module_conda'"
+    echo "The config for this platform specifies a AF_METHOD which is not one of 'conda' or 'module_conda'"
     exit -1
 fi
 
 # Use the existing environment if it can be found
-conda_env_path=`conda env list | grep "${HM_AF_ENV_NAME}" | awk '{print $2}'`
+conda_env_path=`conda env list | grep "${AF_ENV_NAME}" | awk '{print $2}'`
 if [ "${conda_env_path}" == "" ]; then
-    if [ -e ${conda_env_root}/${HM_AF_ENV_NAME}/bin/python ]; then
-	conda_env_path=${conda_env_root}/${HM_AF_ENV_NAME}
+    if [ -e ${conda_env_root}/${AF_ENV_NAME}/bin/python ]; then
+	conda_env_path=${conda_env_root}/${AF_ENV_NAME}
     fi
 fi
 
 # Create the environment if necessary
 if [ "${conda_env_path}" == "" ]; then
-    conda_env_path=${conda_env_root}/${HM_AF_ENV_NAME}
+    conda_env_path=${conda_env_root}/${AF_ENV_NAME}
     echo 'Creating the conda environment'
     conda create --yes --prefix ${conda_env_path} python=${python_version} pip
 else
