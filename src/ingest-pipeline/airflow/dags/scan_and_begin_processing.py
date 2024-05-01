5169f829cfdc922f7e543e87bb3769129f06b4ea
David Betancur <dbetancur@psc.edu>
2024-05-01 13:10:39 -0400

    Suspending submodule call since it isn't module callable yet

--- src/ingest-pipeline/airflow/dags/scan_and_begin_processing.py
+++ src/ingest-pipeline/airflow/dags/scan_and_begin_processing.py
@@ -20,7 +20,7 @@ from airflow.exceptions import AirflowException
 from airflow.operators.python import PythonOperator
 
 sys.path.append(airflow_conf.as_dict()["connections"]["SRC_PATH"].strip("'").strip('"'))
-from submodules import atlasd2k_prepare_replicate
+# from submodules import atlasd2k_prepare_replicate
 
 
 sys.path.pop()
@@ -53,7 +53,8 @@ with HMDAG(
 
     def download_replicate(**kwargs):
         try:
-            replicate = atlasd2k_prepare_replicate(server_name="", args={"replicate": kwargs["replicate"]})
+            # replicate = atlasd2k_prepare_replicate(server_name="", args={"replicate": kwargs["replicate"]})
+            pass
         except Exception as e:
             raise AirflowException(e)
 
