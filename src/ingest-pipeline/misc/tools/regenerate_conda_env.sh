8e1baad6d95d17de221836ba4ef31ae31a24d3d4
David Betancur <dbetancur@psc.edu>
2024-05-30 14:59:42 -0400

    Bugfixing removing platform dependency not needed with Slurm.

--- src/ingest-pipeline/misc/tools/regenerate_conda_env.sh
+++ src/ingest-pipeline/misc/tools/regenerate_conda_env.sh
@@ -33,6 +33,15 @@ function get_dir_of_this_script () {
 get_dir_of_this_script  # sets $DIR
 cd $DIR
 
+platform_file="$DIR/airflow_environments/env_${NIDDK_INSTANCE}.sh"
+
+if [[ -e "${platform_file}" ]] ; then
+    source "${platform_file}"
+else
+    echo "Platform configuration file ${platform_file} does not exist"
+    exit -1
+fi
+
 echo $AF_METHOD $AF_ENV_NAME
 if [ "${AF_METHOD}" == 'conda' ] ; then
     which conda || export PATH=/opt/anaconda3/bin:$PATH
